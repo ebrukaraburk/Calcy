66a177f98a193f1650c82f88d3436ab0ae632a56
ebrukaraburk <117065211+ebrukaraburk@users.noreply.github.com>
2023-03-26 21:48:21 +0300

    ddeneme

--- app/src/androidTest/java/com/example/yenihesapmakinesi/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/yenihesapmakinesi/ExampleInstrumentedTest.java
@@ -18,8 +18,8 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
+    public void   suseAppContext() {
+        // Context of  the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.yenihesapmakinesi", appContext.getPackageName());
     }
